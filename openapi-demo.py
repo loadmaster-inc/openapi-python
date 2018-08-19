4754b282e7fa163f9a56e88ba6e15511282dbf97
LoadMaster <info@zhuangxiang.com>
2018-08-19 22:18:31 +0800

    LoadMaster OpenAPI python demo

---
+++ openapi-demo.py
@@ -0,0 +1,44 @@
+import json
+import requests
+
+
+if __name__ == '__main__':
+    response = requests.post('https://api.zhuangxiang.com/connect/token', data={
+        'client_id': 'your app id',
+        'client_secret': 'your app secret',
+        'grant_type': 'password',
+        'username': 'your account name(admin)',
+        'password': 'your account password'
+    }, cookies={
+        'Abp.TenantId': 'your tenant id'
+    })
+    access_token = json.loads(response.text)['access_token']
+    taskdata = {
+        'type': 0,
+        'packingCargoes': [{
+            'name': 'cargo1',
+            'length': 1.1,
+            'width': 0.8,
+            'height': 0.6,
+            'weight': 0.5,
+            'quantity': 99
+        }],
+        'packingContainers': [{
+            'name': "20ft",
+            'InnerX': 2.35,
+            'InnerY': 2.38,
+            'InnerZ': 5.89,
+            'Maxload': 21000
+        }],
+        'interimContainers': [],
+        'loadingOptions': {},
+        'interimOptions': {},
+        'predefinedModels': [],
+        'pointedContainers': [],
+        'skuCargoes': []
+    }
+    r = requests.post('https://optimize.zhuangxiang.com/api/LoadingOptimize/DoLoadingOptimize', data=json.dumps(taskdata), headers={
+        'Authorization': 'bearer ' + access_token,
+        'content-type': 'application/json'
+    })
+    print(r.text)
---
+++ readme.md
@@ -0,0 +1,13 @@
+# LoadMaster OpenAPI python demo
+
+Requirements:
+
+```
+pip install requests
+```
+
+Usage:
+
+```
+python openapi-demo.py
+```
